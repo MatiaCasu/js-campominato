b9f7a9c40e1546c3bddb952c92cf6fe1aa3d6b48
Matia <matia.casu@gmail.com>
2021-03-09 20:37:41 +0100

    Aggiunto ciclo di prompt che finisce con bomba/ partiteMax

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/* Il computer deve generare 16 numeri casuali tra 1 e 100.
-I numeri non possono essere duplicati.
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
-
 // Funzione numeri random tra Min e Max
 function nRandom(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,21 +5,54 @@ function nRandom(min, max){
 // /Funzione numeri random tra Min e Max
 
 // Dichiarazioni Variabili
-var bombeLength = 16;
-var bombe = [];
+var bombeListaMax = 16;
+var maxRange = 100;
+var bombeLista = [];
 var nBomba = 0;
+var nUtente = 0;
+var nScelti = [];
+var maxPartite = maxRange - bombeListaMax;
 // /Dichiarazioni Variabili
 
-// Crea una lista di numeri random diversi tra loro
+// Crea una lista(16) di numeri random(1-100) diversi tra loro
 do{
-  nBomba = nRandom(1, 100);
-  if(bombe.includes(nBomba)){
-    nBomba = nRandom(1, 100);
+  nBomba = nRandom(1, maxRange);
+  if(bombeLista.includes(nBomba)){
+    nBomba = nRandom(1, maxRange);
   }
   else{
-    bombe.push(nBomba);
+    bombeLista.push(nBomba);
   }
 }
-while(bombe.length < bombeLength)
-console.log(bombe);
-// Crea una lista di numeri random diversi tra loro
+while(bombeLista.length < bombeListaMax);
+console.log(bombeLista);
+// Crea una lista(16) di numeri random(1-100) diversi tra loro
+
+
+// Chiedi all’utente un numero 1-100 fin che l'array nScelti non ha il valore di ciclofinito
+do{
+  nUtente = parseInt(prompt("Inserisci un numero da 1 a " + maxRange));
+  // Se nUtente è incluso nell'array bombe cambia il valore di nSceltiLength per terminare il ciclo
+  if(bombeLista.includes(nUtente)){
+    alert("booom!");
+    nScelti.length = maxPartite + 1;
+  }
+  // Se nUtente è incluso nell'array di nScelti o fuori range
+  else if(nScelti.includes(nUtente) || nUtente < 1 || nUtente > maxRange || isNaN(nUtente)){
+    alert("Numero non valido!");
+  }
+  // Se nUtente ha superato le validazioni si aggiunge all'array di numeri già scelti
+  else{
+    nScelti.push(nUtente);
+  }
+  console.log(nScelti );
+}
+while(nScelti.length < maxPartite);
+
+if (nScelti.length === maxPartite){
+  alert("Hai vinto!");
+}
+else{
+  alert("Hai perso!");
+}
+// Chiedi all’utente un numero 1-100 fin che l'array nScelti non ha il valore di ciclofinito
