afd80fa7f7381664c54e96bd644453ec0119501e
Matia <matia.casu@gmail.com>
2021-03-09 16:44:44 +0100

    Lista numeri Random

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* Il computer deve generare 16 numeri casuali tra 1 e 100.
+I numeri non possono essere duplicati.
+In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+L’utente non può inserire più volte lo stesso numero.
+Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
+
+// Funzione numeri random tra Min e Max
+function nRandom(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// /Funzione numeri random tra Min e Max
+
+// Dichiarazioni Variabili
+var bombeLength = 5;
+var bombe = [];
+var nBomba = 0;
+// /Dichiarazioni Variabili
+
+// Crea una lista di numeri random diversi tra loro
+do{
+  nBomba = nRandom(1, bombeLength);
+  if(bombe.includes(nBomba)){
+    nBomba = nRandom(1, bombeLength);
+  }
+  else{
+    bombe.push(nBomba);
+  }
+}
+while(bombe.length < bombeLength)
+console.log(bombe);
+// Crea una lista di numeri random diversi tra loro
