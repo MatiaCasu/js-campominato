6d240d4242e8f4a339dbeb224fc813e610075cfa
Matia <matia.casu@gmail.com>
2021-03-10 14:53:34 +0100

    Aggiunta scelta livello con Switch

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-/* Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-BONUS: (da fare solo se funziona tutto il resto)
-all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-con difficoltà 0 => tra 1 e 100
-con difficoltà 1 => tra 1 e 80
-con difficoltà 2 => tra 1 e 50 */
-
-
-
 // Funzione numeri random tra Min e Max(compresi)
 function nRandom(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,13 +6,40 @@ function nRandom(min, max){
 
 // Dichiarazioni Variabili
 var bombeListaMax = 3;
-var maxRange = 10;
+var maxRange;
 var bombeLista = [];
 var nBomba = 0;
 var nUtente = 0;
 var nScelti = [];
-var maxPartite = maxRange - bombeListaMax;
 // /Dichiarazioni Variabili
+
+/* BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e maxRange = 100
+con difficoltà 1 => tra 1 e maxRange = 80
+con difficoltà 2 => tra 1 e maxRange = 50 */
+
+
+// Livello
+do {
+  var livello = parseInt(prompt("Scegli il livello: 0  1  2"));
+  switch (livello) {
+    case 0:
+      maxRange = 10;
+      break;
+    case 1:
+      maxRange = 8;
+      break;
+    case 2:
+      maxRange = 5;
+      break;
+    default:
+      alert("Scegli tra 0 1 o 2!")
+      livello = false;
+  }
+} while (livello === false);
+// /Livello
+
 
 // Crea una lista(16) di numeri random(1-100) diversi tra loro
 do{
@@ -37,7 +55,12 @@ while(bombeLista.length < bombeListaMax);
 console.log(bombeLista);
 // Crea una lista(16) di numeri random(1-100) diversi tra loro
 
+
+// Variabili del ciclo nUtente
 var esplosione = false;
+var maxPartite = maxRange - bombeListaMax; //Variabile definibile solo dopo aver scelto il livello e relativo range massimo
+// Variabili del ciclo nUtente
+
 // Chiedi all’utente un numero 1-100 fin che l'array nScelti non ha il valore di partite massime o esplosione diventa vera
 do{
   nUtente = parseInt(prompt("Inserisci un numero da 1 a " + maxRange));
@@ -68,5 +91,5 @@ else{
 // Finito il ciclo se la bomba non è esplosa "Hai vinto"
 
 // Il punteggio è dato dai numeri validi inseriti dall'utente
-console.log("Il tuo punteggio è: " + nScelti.length);
+console.log("Il tuo punteggio è: " + nScelti.length + " su " + maxPartite);
 // Il punteggio è dato dai numeri validi inseriti dall'utente
